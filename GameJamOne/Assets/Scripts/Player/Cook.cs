77fadb816e586eaf9cd0d2e6b69348aa3bf5e4df
edohtron <edohtron@users.noreply.github.com>
2018-03-07 02:51:15 +1100

    fixed shooting sorta

--- GameJamOne/Assets/Scripts/Player/Cook.cs
+++ GameJamOne/Assets/Scripts/Player/Cook.cs
@@ -1,16 +1,201 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using XboxCtrlrInput;
 
 public class Cook : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private PlayerController sr_pController;
+    private Foods sr_food;
+    //private GameObject chefferson;
+    public XboxController controller;
+
+    public GameObject foodA;
+    public GameObject foodB;
+    public GameObject foodX;
+    public GameObject foodY;
+    public Image chopDisplay;
+    public int chopCount;
+    public int chopMax;
+    
+    public bool doneChopping;
+    
+    // SCRIPT REFERENCE
+
+
+    private void Awake()
+    {
+        sr_pController = GetComponent<PlayerController>();
+        sr_food = GetComponent<Foods>();
+
+        if (gameObject.tag == "Player1")
+        {
+            controller = XboxController.First;
+        }
+        else if (gameObject.tag == "Player2")
+        {
+            controller = XboxController.Second;
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        doneChopping = false;
+        chopCount = 0;
+        chopMax = 6;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        
+
+        //--------------------------------------------------
+        // COOK BUTTONS
+        //--------------------------------------------------
+
+
+        if (XCI.GetButtonDown(XboxButton.A, controller))
+        {
+            if (sr_pController.currentlyOnStove)
+            {
+                Foods temp = sr_pController.currentlyOnStove.GetComponent<Foods>();
+                if (!temp.isReady)
+                {
+                    if ((int)temp.thisFoodType == 0)
+                    {
+                        temp.SetFoodType(1);
+                    }
+                    if ((int)temp.thisFoodType == 1)
+                    {
+                        temp.getChopped();
+                        chopCount += 1;
+                        chopDisplay.fillAmount = chopCount / chopMax;
+                    }
+                }
+                else if (temp.isReady)
+                {
+                    ToggleDone(true);
+                }
+            }
+
+            else
+            {
+                SetFood(foodA, 1);
+            }
+        }
+
+        if (XCI.GetButtonDown(XboxButton.B, controller))
+        {
+            if (sr_pController.currentlyOnStove)
+            {
+                Foods temp = sr_pController.currentlyOnStove.GetComponent<Foods>();
+                if (!temp.isReady)
+                {
+                    chopDisplay.fillAmount = sr_food.chopProgress / sr_food.chopFinish;
+                    if ((int)temp.thisFoodType == 0)
+                    {
+                        temp.SetFoodType(2);
+                    }
+                    if ((int)temp.thisFoodType == 2)
+                    {
+                        temp.getChopped();
+                    }
+                }
+                else if (temp.isReady)
+                {
+                    ToggleDone(true);
+                }
+            }
+
+            else
+            {
+                SetFood(foodB, 2);
+            }
+        }
+
+        if (XCI.GetButtonDown(XboxButton.X, controller))
+        {
+            if (sr_pController.currentlyOnStove)
+            {
+                Foods temp = sr_pController.currentlyOnStove.GetComponent<Foods>();
+                if (!temp.isReady)
+                {
+                    chopDisplay.fillAmount = sr_food.chopProgress / sr_food.chopFinish;
+                    if ((int)temp.thisFoodType == 0)
+                    {
+                        temp.SetFoodType(3);
+                    }
+                    if ((int)temp.thisFoodType == 3)
+                    {
+                        temp.getChopped();
+                    }
+                }
+                else if (temp.isReady)
+                {
+                    ToggleDone(true);
+                }
+            }
+
+            else
+            {
+                SetFood(foodX, 3);
+            }
+        }
+
+        if (XCI.GetButtonDown(XboxButton.Y, controller))
+        {
+            if (sr_pController.currentlyOnStove)
+            {
+                Foods temp = sr_pController.currentlyOnStove.GetComponent<Foods>();
+                if (!temp.isReady)
+                {
+                    chopDisplay.fillAmount = sr_food.chopProgress / sr_food.chopFinish;
+                    if ((int)temp.thisFoodType == 0)
+                    {
+                        temp.SetFoodType(4);
+                    }
+                    if ((int)temp.thisFoodType == 4)
+                    {
+                        temp.getChopped();
+                    }
+                }
+                else if (temp.isReady)
+                {
+                    ToggleDone(true);
+                }
+            }
+
+            else
+            {
+                SetFood(foodY, 4);
+            }
+        }
+    }
+
+    private void SetFood(GameObject food, int val)
+    {
+        sr_pController.canThrow = true;
+        sr_pController.currentFood = food;
+
+        GameObject setFood = Instantiate(sr_pController.currentFood, sr_pController.cookPos.transform) as GameObject;
+        setFood.transform.position = sr_pController.cookPos.transform.position;
+        sr_pController.currentlyOnStove = setFood;
+        sr_pController.currentlyOnStove.GetComponent<Foods>().SetFoodType(val);
+        //set food plop sound?
+    }
+
+    public void ToggleDone(bool tof)
+    {
+        doneChopping = tof;
+       
+    }
+    public void ToggleChopDone(int val)
+    {
+        chopCount = val;
+    }
+
+
 }
--- GameJamOne/Assets/Scripts/Player/PlayerController.cs
+++ GameJamOne/Assets/Scripts/Player/PlayerController.cs
@@ -1,52 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using XboxCtrlrInput;
 
 public class PlayerController : MonoBehaviour
 {
+    private Cook sr_cook;
+    //private Foods sr_food;
+
 
     public XboxController controller;
 
-    // THROW VARS
-    //throw angle must be between 0-1
-    public Vector3 aim;
+    public Vector3 aim; //throw angle must be between 0-1
     public Transform throwPos;
+    public GameObject cookPos;
+
+    public float aMultiplier;
+    public float fMultiplier;
     public int throwPower;
     public float throwCooldown;
     public float throwAngle;
     public float throwForce;
     public float throwCharge = 0;
+    public float minCharge;
+    public float maxCharge;
+    private bool initiateCharge = false;
     public bool charging = false;
     public bool canThrow = false;
-
-    // COOK VARS
-    public GameObject cookPos;
+    public GameObject currentlyOnStove;
     public GameObject currentFood;
-    protected GameObject currentlyOnStove;
-    public GameObject foodA;
-    public GameObject foodB;
-    public GameObject foodX;
-    public GameObject foodY;
-    public bool isCooking;
-
-    protected Foods foodScript;
 
     private void Awake()
     {
-        //setFoodType = currentFood.GetComponent<Foods>();
+        sr_cook = gameObject.GetComponent<Cook>();
+        //sr_food = gameObject.GetComponent<Foods>();
+
+        if (gameObject.tag == "Player1")
+        {
+            controller = XboxController.First;
+        }
+        else if (gameObject.tag == "Player2")
+        {
+            controller = XboxController.Second;
+        }
+
     }
 
     // Use this for initialization
 
     void Start()
     {
-
+        minCharge = 0.2f;
+        maxCharge = 2.5f;
         throwCooldown = 0;
-        throwForce = 500.0f;
-        isCooking = false;
+        throwForce = 425.0f;
+        throwAngle = 0.5f;
         currentFood = null;
-
 
     }
 
@@ -62,87 +72,6 @@ public class PlayerController : MonoBehaviour
             throwCooldown = 0;
         }
 
-        //--------------------------------------------------
-        // COOK BUTTONS
-        //--------------------------------------------------
-
-
-        if (XCI.GetButtonDown(XboxButton.A, controller))
-        {
-            if (currentlyOnStove)
-            {
-                Foods temp = currentlyOnStove.GetComponent<Foods>();
-                if((int)temp.thisFoodType == 4)
-                {
-                    temp.SetFoodType(0);
-                }
-                if ((int)temp.thisFoodType == 0)
-                {
-                    temp.Chop();
-                }
-            }
-            else
-            {
-                SetFood(foodA, 0);                
-            }
-        }
-        if (XCI.GetButtonDown(XboxButton.B, controller))
-        {
-            if (currentlyOnStove)
-            {
-                Foods temp = currentlyOnStove.GetComponent<Foods>();
-                if ((int)temp.thisFoodType == 4)
-                {
-                    temp.SetFoodType(1);
-                }
-                if ((int)temp.thisFoodType == 1)
-                {
-                    temp.Chop();
-                }
-            }
-            else
-            {
-                SetFood(foodB, 1);
-            }
-        }
-        if (XCI.GetButtonDown(XboxButton.X, controller))
-        {
-            if (currentlyOnStove)
-            {
-                Foods temp = currentlyOnStove.GetComponent<Foods>();
-                if ((int)temp.thisFoodType == 4)
-                {
-                    temp.SetFoodType(2);
-                }
-                if ((int)temp.thisFoodType == 2)
-                {
-                    temp.Chop();
-                }
-            }
-            else
-            {
-                SetFood(foodX, 2);
-            }
-        }
-        if (XCI.GetButtonDown(XboxButton.Y, controller))
-        {
-            if (currentlyOnStove)
-            {
-                Foods temp = currentlyOnStove.GetComponent<Foods>();
-                if ((int)temp.thisFoodType == 4)
-                {
-                    temp.SetFoodType(3);
-                }
-                if ((int)temp.thisFoodType == 3)
-                {
-                    temp.Chop();
-                }
-            }
-            else
-            {
-                SetFood(foodY, 3);
-            }
-        }
 
 
         //--------------------------------------------------
@@ -164,42 +93,64 @@ public class PlayerController : MonoBehaviour
             Vector3 up = new Vector3(0, 0.2f);
 
             Debug.DrawRay(this.transform.position + up, aim);
+        }
 
-
-            charging = true;
-            if (charging)
+        if ((throwCooldown <= 0 && sr_cook.doneChopping) && 
+           ((XCI.GetButtonDown(XboxButton.LeftBumper, controller)) || (XCI.GetButtonDown(XboxButton.RightBumper, controller))))
+        {
+            if (initiateCharge)
             {
-                throwCharge += Time.deltaTime;
+
+                //ThrowFood(currentFood, throwPos.position, aMultiplier, fMultiplier);
+                if (throwCharge <= 0.9f)
+                {
+
+                    throwCharge = Mathf.Clamp(throwCharge, 0.55f, 0.9f);
+                    Debug.Log(throwCharge);
+                    ThrowFood(currentFood, throwPos.position, aMultiplier, fMultiplier * throwCharge);
+                   // throwCharge = 0;
+                   // initiateCharge = false;
+                }
+                if (throwCharge > 0.9f && throwCharge <= 1.35f)
+                {
+                   throwCharge = Mathf.Clamp(throwCharge, 0.9f, 1.3f);
+                   Debug.Log(throwCharge);
+                   ThrowFood(currentFood, throwPos.position, aMultiplier, fMultiplier * throwCharge);
+                }
+                
+                if (throwCharge > 1.35f)
+                {
+                   throwCharge = Mathf.Clamp(throwCharge, 1.35f, 1.4f);
+                   Debug.Log(throwCharge);
+                   ThrowFood(currentFood, throwPos.position, aMultiplier, fMultiplier * throwCharge);
+                }
+            }
+
+            else
+            {
+               
+                initiateCharge = true;
+
                 if (currentFood != null)
                 {
                     canThrow = true;
                 }
-
+                
             }
-
-            if (throwCooldown <= 0 && ((XCI.GetButton(XboxButton.LeftBumper, controller)) || (XCI.GetButton(XboxButton.RightBumper, controller))))
-            {
-                if (throwCharge <= 0.5)
-                {
-                    ThrowFood(currentFood, throwPos.position, 0.1f);
-                }
-
-                if (throwCharge > 0.5f && throwCharge < 1.5f)
-                {
-                    ThrowFood(currentFood, throwPos.position, 0.3f);
-                }
-
-                if (throwCharge >= 1.5f)
-                {
-                    ThrowFood(currentFood, throwPos.position, 0.5f);
-                }
-
-            }
+                       
         }
 
         else if (leftInput.x == 0 || leftInput.y == 0)
         {
             ResetAim();
+        }
+
+        if (initiateCharge)
+        {
+        
+           throwCharge += Time.deltaTime;
+           //aMultiplier += Time.deltaTime;
+           //fMultiplier += Time.deltaTime;
         }
     }
 
@@ -208,46 +159,39 @@ public class PlayerController : MonoBehaviour
         aim.x = 0;
         aim.y = 0;
         aim.z = 0;
-        charging = false;
-        canThrow = false;
-        throwCharge = 0;
-        throwAngle = 0;
+
     }
     
 
-    private void ThrowFood(GameObject food, Vector3 sPos, float throwAngle)
+    private void ThrowFood(GameObject food, Vector3 sPos, float throwAngle, float throwForce)
     {
         Vector3 actualAim = aim;
         actualAim.y = throwAngle;
         actualAim.Normalize();
         // GameObject newFood = Instantiate(currentFood, sPos, Quaternion.Euler(actualAim)) as GameObject;
-
+    
         if (currentlyOnStove != null && currentlyOnStove.GetComponent<Foods>().isReady)
         {
             currentlyOnStove.transform.position = throwPos.position;
             currentlyOnStove.transform.Rotate(Vector3.right, throwAngle);
             currentlyOnStove.GetComponent<Rigidbody>().AddForce(actualAim * throwForce);
             throwCooldown = 1.0f;
+            throwAngle = 0;
+            throwCharge = 0;
             currentFood = null;
             currentlyOnStove = null;
+            initiateCharge = false;
+            canThrow = false;
+            sr_cook.ToggleDone(false);
+            sr_cook.ToggleChopDone(0);
             ResetAim();
+            
             //throw sound
         }
+
+        throwCharge = 0;
+        initiateCharge = false;
     }
-
-    private void SetFood(GameObject food, int val)
-    {
-        isCooking = true;
-        canThrow = true;
-        currentFood = food;
-
-        GameObject setFood = Instantiate(currentFood, cookPos.transform) as GameObject;
-        setFood.transform.position = cookPos.transform.position;
-        currentlyOnStove = setFood;
-        currentlyOnStove.GetComponent<Foods>().SetFoodType(val);
-        //set food plop sound?
-    }
-
-  
+      
 
 }
